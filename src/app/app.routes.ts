1e24f7af9baef58d810b3cdc56016bb2f7a5d337
Rishi Airan <20bcs042@ietdavv.edu.in>
2024-10-03 23:17:09 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"header",
+        component:HeaderComponent
+    },
+    {
+        path:"footer",
+        component:FooterComponent
+    }
+];
